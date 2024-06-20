97377906449d77333820986c88e483e4819d71ac
goshaaaa777 <isaiasddx322z@gmail.com>
2024-06-19 21:50:50 -0500

    first commit

---
+++ src/main/java/cibertec/edu/api_rest_ventas/ApiRestVentasApplication.java
@@ -0,0 +1,13 @@
+package cibertec.edu.api_rest_ventas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestVentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestVentasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cibertec/edu/api_rest_ventas/ApiRestVentasApplicationTests.java
@@ -0,0 +1,13 @@
+package cibertec.edu.api_rest_ventas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestVentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
